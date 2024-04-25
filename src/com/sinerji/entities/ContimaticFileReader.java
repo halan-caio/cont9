4c3614b32d6ecb71308f3707c16a40a3645f5e04
Halan Caio <hallankayo20@gmail.com>
2024-04-25 15:47:41 -0300

    created contimaticFileReader method

--- src/com/sinerji/entities/ContimaticFileReader.java
+++ src/com/sinerji/entities/ContimaticFileReader.java
@@ -1,5 +1,8 @@
 package com.sinerji.entities;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,7 +10,17 @@ import com.sinerji.entities.interfaces.CSVFileReader;
 
 public class ContimaticFileReader implements CSVFileReader {
     public List<String> readLines(String filePath) {
-        // Implementar leitura de arquivo Contimatic
-        return new ArrayList<>();
+    	List<String> lines = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return lines;
     }
 }
