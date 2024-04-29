d77aa8932d0fc5327fc3b8ed6108b4afa19cd67e
Halan Caio <hallankayo20@gmail.com>
2024-04-29 16:03:13 -0300

    Correção da função de comparação para dividir String em Lista

--- src/com/sinerji/entities/ContimaticFileReader.java
+++ src/com/sinerji/entities/ContimaticFileReader.java
@@ -11,16 +11,27 @@ import com.sinerji.entities.interfaces.CSVFileReader;
 public class ContimaticFileReader implements CSVFileReader {
     public List<String> readLines(String filePath) {
     	List<String> lines = new ArrayList<>();
+		final String[] PREFIXES = { "|0000|", "|C100|", "|C190|", "|D100|", "|D190|" };
 
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                lines.add(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				// Verifica se a linha começa com algum dos prefixos desejados
+				boolean validLine = false;
+				for (String prefix : PREFIXES) {
+					if (line.startsWith(prefix)) {
+						validLine = true;
+						break; // Não é necessário verificar outros prefixos
+					}
+				}
+				if (validLine) {
+					lines.add(line);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        return lines;
+		return lines;
     }
 }
--- src/com/sinerji/entities/LineByLineComparisonStrategy.java
+++ src/com/sinerji/entities/LineByLineComparisonStrategy.java
@@ -5,32 +5,36 @@ import java.util.List;
 
 import com.sinerji.entities.interfaces.ComparisonStrategy;
 
-public class LineByLineComparisonStrategy  implements ComparisonStrategy {
-	 public List<Difference> compare(List<String> fileOneLines, List<String> fileTwoLines) {
-	        List<Difference> differences = new ArrayList<>();
-	        int numLines = Math.min(fileOneLines.size(), fileTwoLines.size());
+public class LineByLineComparisonStrategy implements ComparisonStrategy {
+	public List<Difference> compare(List<String> fileOneLines, List<String> fileTwoLines) {
+		List<Difference> differences = new ArrayList<>();
+		int numLines = Math.min(fileOneLines.size(), fileTwoLines.size());
+		
+		for (int i = 0; i < numLines; i++) {
+			String[] lineOne = fileOneLines.get(i).split("\\|");
+			String[] lineTwo = fileTwoLines.get(i).split("\\|");
+			if (!lineOne.equals(lineTwo)) {
+				int minLength = Math.min(lineOne.length, lineTwo.length);
+				for (int j = 0; j < minLength; j++) {
+					if (!lineOne[j].equals(lineTwo[j])) {
+						differences.add(new Difference(lineOne[1], j,
+								String.valueOf(lineOne[j]), String.valueOf(lineTwo[j])));
+					}
+				}
+				if (lineOne.length != lineTwo.length) {
+					int longerLength = Math.max(lineOne.length, lineTwo.length);
+					for (int j = minLength; j < longerLength; j++) {
+						String charFromLonger = lineOne.length > lineTwo.length ? lineOne[j]
+								: lineTwo[j];
+						differences.add(new Difference(lineOne[1], j,
+								lineOne.length > lineTwo.length ? String.valueOf(charFromLonger) : "",
+								lineOne.length > lineTwo.length ? "" : String.valueOf(charFromLonger)));
+					}
+				}
+			}
+		}
 
-	        for (int i = 0; i < numLines; i++) {
-	            String lineOne = fileOneLines.get(i);
-	            String lineTwo = fileTwoLines.get(i);
-	            if (!lineOne.equals(lineTwo)) {
-	                int minLength = Math.min(lineOne.length(), lineTwo.length());
-	                for (int j = 0; j < minLength; j++) {
-	                    if (lineOne.charAt(j) != lineTwo.charAt(j)) {
-	                        differences.add(new Difference(lineOne.substring(0, 6), j + 1, String.valueOf(lineOne.charAt(j)), String.valueOf(lineTwo.charAt(j))));
-	                    }
-	                }
-	                if (lineOne.length() != lineTwo.length()) {
-	                    int longerLength = Math.max(lineOne.length(), lineTwo.length());
-	                    for (int j = minLength; j < longerLength; j++) {
-	                        char charFromLonger = lineOne.length() > lineTwo.length() ? lineOne.charAt(j) : lineTwo.charAt(j);
-	                        differences.add(new Difference(lineOne.substring(0, 6), j + 1, lineOne.length() > lineTwo.length() ? String.valueOf(charFromLonger) : "", lineOne.length() > lineTwo.length() ? "" : String.valueOf(charFromLonger)));
-	                    }
-	                }
-	            }
-	        }
-
-	        return differences;
-	    }
+		return differences;
+	}
 
 }
