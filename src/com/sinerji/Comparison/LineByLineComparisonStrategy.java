014318334f35f3ae6675225241a0f527551afd3a
Halan Caio <hallankayo20@gmail.com>
2024-04-30 15:46:43 -0300

    implementation of the areEquals and areEqualDouble functions

--- src/com/sinerji/Comparison/LineByLineComparisonStrategy.java
+++ src/com/sinerji/Comparison/LineByLineComparisonStrategy.java
@@ -9,32 +9,58 @@ public class LineByLineComparisonStrategy implements ComparisonStrategy {
 	public List<Difference> compare(List<String> fileOneLines, List<String> fileTwoLines) {
 		List<Difference> differences = new ArrayList<>();
 		int numLines = Math.min(fileOneLines.size(), fileTwoLines.size());
-		
+
 		for (int i = 0; i < numLines; i++) {
 			String[] lineOne = fileOneLines.get(i).split("\\|");
 			String[] lineTwo = fileTwoLines.get(i).split("\\|");
 			if (!lineOne.equals(lineTwo)) {
 				int minLength = Math.min(lineOne.length, lineTwo.length);
 				for (int j = 0; j < minLength; j++) {
-					if (!lineOne[j].equals(lineTwo[j])) {
-						differences.add(new Difference(lineOne[1], j,
-								String.valueOf(lineOne[j]), String.valueOf(lineTwo[j])));
+					if (!lineOne[j].equals(lineTwo[j]) && !areEquals(lineOne[j], lineTwo[j]) 
+							&& !areEqualDouble(lineOne[j], lineTwo[j])) {
+						differences.add(
+								new Difference(lineOne[1], j, String.valueOf(lineOne[j]), String.valueOf(lineTwo[j])));
 					}
 				}
-				if (lineOne.length != lineTwo.length) {
-					int longerLength = Math.max(lineOne.length, lineTwo.length);
-					for (int j = minLength; j < longerLength; j++) {
-						String charFromLonger = lineOne.length > lineTwo.length ? lineOne[j]
-								: lineTwo[j];
-						differences.add(new Difference(lineOne[1], j,
-								lineOne.length > lineTwo.length ? String.valueOf(charFromLonger) : "",
-								lineOne.length > lineTwo.length ? "" : String.valueOf(charFromLonger)));
-					}
-				}
+
+// ------------ Caso alguma linha seja maior que a outra.
+//				if (lineOne.length != lineTwo.length) {
+//					int longerLength = Math.max(lineOne.length, lineTwo.length);
+//					for (int j = minLength; j < longerLength; j++) {
+//						String charFromLonger = lineOne.length > lineTwo.length ? lineOne[j]
+//								: lineTwo[j];
+//						differences.add(new Difference(lineOne[1], j,
+//								lineOne.length > lineTwo.length ? String.valueOf(charFromLonger) : "",
+//								lineOne.length > lineTwo.length ? "" : String.valueOf(charFromLonger)));
+//					}
+// ------------ }
 			}
 		}
 
 		return differences;
+	}
+
+	private boolean areEquals(String SPEDFileData, String ContimaticFileData) {
+		if ((SPEDFileData.equals("") && ContimaticFileData.equals("0"))
+				|| (ContimaticFileData.equals("") && SPEDFileData.equals("0"))) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean areEqualDouble(String str1, String str2) {
+		try {
+	        // Convertendo as strings em números double
+	        double num1 = Double.parseDouble(str1.replace(',', '.'));
+	        double num2 = Double.parseDouble(str2.replace(',', '.'));
+
+	        // Comparando os números com uma pequena tolerância
+	        double tolerance = 0.000001;
+	        return Math.abs(num1 - num2) < tolerance;
+	    } catch (NumberFormatException e) {
+	        // Se as strings não puderem ser convertidas em números double, elas são consideradas diferentes
+	        return false;
+	    }
 	}
 
 }
--- src/com/sinerji/readers/SPEDFileReader.java
+++ src/com/sinerji/readers/SPEDFileReader.java
@@ -1,37 +1,5 @@
 package com.sinerji.readers;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.sinerji.readers.interfaces.CSVFileReader;
-
-public class SPEDFileReader implements CSVFileReader {
-	public List<String> readLines(String filePath) {
-		List<String> lines = new ArrayList<>();
-		final String[] PREFIXES = { "|0000|", "|C100|", "|C190|", "|D100|", "|D190|" };
-
-		try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-			String line;
-			while ((line = br.readLine()) != null) {
-				// Verifica se a linha começa com algum dos prefixos desejados
-				boolean validLine = false;
-				for (String prefix : PREFIXES) {
-					if (line.startsWith(prefix)) {
-						validLine = true;
-						break; // Não é necessário verificar outros prefixos
-					}
-				}
-				if (validLine) {
-					lines.add(line);
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		return lines;
-	}
+public class SPEDFileReader extends AbstractCSVFileReader {
+	
 }
